201eb115cda7dee923f4b2ee8ff4741d33756360
sumin <emamgin@gmail.com>
2025-08-31 18:18:04 +0900

    연결 링크 수정

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // Axios 인스턴스 생성
 const client = axios.create({
   // API 명세서에 명시된 백엔드 서버 주소를 입력합니다.
-  baseURL: 'http://localhost:8080/hnn', // 예시 URL입니다. 실제 주소에 맞게 수정해주세요.
+  baseURL: 'https://backend-production-2949.up.railway.app', // 예시 URL입니다. 실제 주소에 맞게 수정해주세요.
   timeout: 10000, // 요청 타임아웃
 });
 
